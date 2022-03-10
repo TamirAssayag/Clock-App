9e66ae65d15dca74002d7a19861210b70ffc794a
bonjour <tamirassayag@gmail.com>
2022-03-10 21:46:51 +0200

    removal of scale animation background in mobile

--- src/components/Ui/Background/Background.tsx
+++ src/components/Ui/Background/Background.tsx
@@ -3,34 +3,37 @@ import { userSettings } from "../../../zustand";
 import { AnimatePresence, Variants } from "framer-motion";
 import "./Background.scss";
 import { UiImage } from "../UiImage";
-
-const variants: Variants = {
-  enter: {
-    scale: 0.5,
-    opacity: 0,
-  },
-  center: {
-    opacity: 1,
-    scale: 1,
-  },
-  exit: {
-    opacity: 0,
-    scale: 1.2,
-  },
-};
-
-const framerBgProps = {
-  variants,
-  initial: "enter",
-  animate: "center",
-  exit: "exit",
-  transition: {
-    x: { type: "spring", stiffness: 300, damping: 30 },
-    opacity: { duration: 0.5 },
-  },
-};
+import { useMobile } from "../../../hooks";
 
 export const Background: FC = () => {
+  const { isMobile } = useMobile();
+
+  const variants: Variants = {
+    enter: {
+      scale: !isMobile ? 0.7 : "unset",
+      opacity: 0,
+    },
+    center: {
+      opacity: 1,
+      scale: !isMobile ? 1 : "unset",
+    },
+    exit: {
+      opacity: 0,
+      scale: !isMobile ? 1.2 : "unset",
+    },
+  };
+
+  const framerBgProps = {
+    variants,
+    initial: "enter",
+    animate: "center",
+    exit: "exit",
+    transition: {
+      x: { type: "spring", stiffness: 300, damping: 30 },
+      opacity: { duration: 0.5 },
+    },
+  };
+
   const { data, backgroundImages } = userSettings();
 
   // If no selected user background, use the default background (first in current Images array)
